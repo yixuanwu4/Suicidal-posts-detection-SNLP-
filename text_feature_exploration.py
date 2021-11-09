9674c2d5ef4fcd04c4deb23ed12a9e0befbd8d4d
Yixuan <56627720+yixuanwu4@users.noreply.github.com>
2021-11-09 12:04:12 +0100

    Finish post length plot (figure is saved)

--- text_feature_exploration.py
+++ text_feature_exploration.py
@@ -7,6 +7,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from PIL import Image
+from statistics import mean
 
 
 
@@ -48,9 +49,45 @@ def plot_most_used_words(dataname, data_series, palette):
     plt.ylabel('Common Words in ' + dataname, fontsize=9)
     plt.savefig('/home/nauxiy/Workspace/Suicidal-posts-detection-SNLP-/plots/' + dataname + ".png")
 
-    plt.show()
+    # plt.show()
     
 
 plot_most_used_words("Common Words in Suicidal Posts", suicide_posts, palette="ocean_r")
-plot_most_used_words("Common Words in Depression Posts", depress_posts, palette="ocean_r")
+plot_most_used_words("Common Words in Depression Posts", depress_posts, palette="magma")
 plot_most_used_words("Common Words in General Posts", general_posts, palette="ocean_r")
+
+# COLLECT EACT POST'S LENGTH
+sui_selftext_length = [len(suicide_posts[i]) for i in range(len(suicide_posts))]
+dep_selftext_length = [len(depress_posts[i]) for i in range(len(suicide_posts))]
+gen_selftext_length = [len(general_posts[i]) for i in range(len(suicide_posts))]
+
+# CALCULATE AVERAGE POST LENGTH IN EACH CLASS
+ave_sui_selftext_length = mean(sui_selftext_length)
+ave_dep_selftext_length = mean(dep_selftext_length)
+ave_gen_selftext_length = mean(gen_selftext_length)
+
+print("__________________________________")
+print("Average length of a suicidal post: {}".format(ave_sui_selftext_length))
+print("Average length of a depression post: {}".format(ave_dep_selftext_length))
+print("Average length of a general post: {}".format(ave_gen_selftext_length))
+
+
+# MAPPING POST LENGTH WITH THE LABEL
+all_post_length = sui_selftext_length + dep_selftext_length + gen_selftext_length
+all_labels = suicide_label + depress_label + general_label 
+label_post_dict = {'post length':all_post_length, 'labels': all_labels}
+df = pd.DataFrame(label_post_dict)
+sns.set_theme(style="ticks", palette="pastel")
+plt.figure(figsize = (18, 12))
+sns.boxplot(data =df,
+               y = 'post length', 
+               x = 'labels',
+               hue = "labels",
+               palette = "magma_r",
+               showfliers = False
+               );
+plt.title("Length of Posts");
+plt.xlabel("posts");
+plt.ylabel("Number of words");
+plt.savefig('/home/nauxiy/Workspace/Suicidal-posts-detection-SNLP-/plots/' + "length_of_posts.png")
+plt.show()
